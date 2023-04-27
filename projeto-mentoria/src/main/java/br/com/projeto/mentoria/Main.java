4ac62e6958ea1a88c6d170a2b9cd804f597dfdb8
aquilesValerio <aquilesvalerio@gmail.com>
2023-04-27 09:06:22 -0300

    first commit

---
+++ projeto-mentoria/src/main/java/br/com/projeto/mentoria/Main.java
@@ -0,0 +1,12 @@
+package br.com.projeto.mentoria;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
